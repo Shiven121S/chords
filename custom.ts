d7747c17de084616d8bb462806bab420b87554ac
LudumStudios <65925982+Shiven121S@users.noreply.github.com>
2021-03-28 22:26:21 -0400

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,67 @@
+//% color="#AA278D" weight=100
+namespace Chords {
+    //% block
+    export function C_Major() {
+        timer.background(function () {
+           music.playTone(262, music.beat(BeatFraction.Half)) 
+        })
+        timer.background(function () {
+            music.playTone(330, music.beat(BeatFraction.Half))
+        })
+        timer.background(function () {
+            music.playTone(392, music.beat(BeatFraction.Half))
+        })
+    }
+
+    //% block
+    export function D_Major() {
+        timer.background(function () {
+           music.playTone(294, music.beat(BeatFraction.Half)) 
+        })
+        timer.background(function () {
+            music.playTone(370, music.beat(BeatFraction.Half))
+        })
+        timer.background(function () {
+            music.playTone(440, music.beat(BeatFraction.Half))
+        })
+    }
+
+    //% block
+    export function E_Major() {
+        timer.background(function () {
+           music.playTone(330, music.beat(BeatFraction.Half)) 
+        })
+        timer.background(function () {
+            music.playTone(415, music.beat(BeatFraction.Half))
+        })
+        timer.background(function () {
+            music.playTone(494, music.beat(BeatFraction.Half))
+        })
+    }
+
+    //% block
+    export function F_Major() {
+        timer.background(function () {
+           music.playTone(349, music.beat(BeatFraction.Half)) 
+        })
+        timer.background(function () {
+            music.playTone(440, music.beat(BeatFraction.Half))
+        })
+        timer.background(function () {
+            music.playTone(523, music.beat(BeatFraction.Half))
+        })
+    }
+
+    //% block
+    export function G_Major() {
+        timer.background(function () {
+           music.playTone(392, music.beat(BeatFraction.Half)) 
+        })
+        timer.background(function () {
+            music.playTone(494, music.beat(BeatFraction.Half))
+        })
+        timer.background(function () {
+            music.playTone(587, music.beat(BeatFraction.Half))
+        })
+    }
+}
